22b3ea189ef88b6f049b04bc5d2d6b9a04c4e625
Nihal Yadav <nihalyadav8524@gmail.com>
2026-01-04 01:20:30 +0530

    add utils for error and response

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express()
+
+// for configuring cors settings
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+// for using json in our app, with a limit of size
+app.use(express.json({
+    limit: "16kb",
+}))
+
+// for url encoding and allowing nested objects in url
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb",
+}))
+
+// for saving static content on the server (temporarily)
+app.use(express.static("public"))
+
+// for configuring cookies
+app.use(cookieParser())
+
+export { app }
